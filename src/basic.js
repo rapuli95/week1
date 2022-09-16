d08664ecbb30f4f8d91dd315f995b07db92f2306
rapuli95 <109424784+rapuli95@users.noreply.github.com>
2022-09-16 15:32:33 +0300

    Update basic.js

--- src/basic.js
+++ src/basic.js
@@ -1,5 +1,7 @@
+document.addEventListener("click",Switchdata (){
+ console.log("hello world")
+})
 
-document.addEventListener("click",function (){
-  const text = document.getElementById("mybutton").innerHTML = My Notebook
-
+document.addEventListener("click",addData(){
+    const text = document.getElementsByTagName("il")
 })
